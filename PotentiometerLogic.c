0494bc58ae030ade5ba180b708e1b9bb3ed1de84
JavierO <javier.ortiz13@upr.edu>
2018-01-14 19:04:38 -0400

    Created Pot logic file

---
+++ PotentiometerLogic.c
@@ -0,0 +1,21 @@
+#include "MotorAndSensorConfig.c"
+
+/*******************************************************************
+
+	Handles Potentiometer Sensor logic and functions
+
+*******************************************************************/
+
+/**********
+Constants
+**********/
+
+//globals
+
+//setters
+
+//getters
+
+/**********
+Methods
+**********/
--- SensorLogic.h
+++ SensorLogic.h
@@ -2,4 +2,5 @@
 
 #include "EncoderLogic.c"
 #include "GyroLogic.c"
+#include "PotentiometerLogic.c"
 //add other sensor's logic as they are made
