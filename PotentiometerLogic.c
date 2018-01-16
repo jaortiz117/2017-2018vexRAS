f8276dbcc417b260041dc5df65a48f7a9238a4bd
JavierO <javier.ortiz13@upr.edu>
2018-01-15 20:21:46 -0400

    added logic functions to potentiometer
    map, setLimits, some constants

--- PotentiometerLogic.c
+++ PotentiometerLogic.c
@@ -9,20 +9,32 @@
 /**********
 Constants
 **********/
+#define POT_MIN 5
+#define POT_MAX 4092 //need to verify
+#define SENSOR_AMOUNT 3
 
 //globals
-//int[] sensors = {[s1:{lowLim, hiLim}], ...};
+int sensors[SENSOR_AMOUNT][2] = {
+	{POT_MIN, POT_MAX},
+	{POT_MIN, POT_MAX},
+	{POT_MIN, POT_MAX}};
 
 //setters
-//void setRange(int lowLimit, int highLimit);
-
+void setRange(tSensors sensor, int lowLimit, int highLimit){//limits in "ticks" NOT degrees
+	sensors[sensor][0] = lowLimit;
+	sensors[sensor][1] = highLimit;
+}
 
 //getters
-//int getPot();
-//int getPotDeg();
+//int getPot(int sensor);
+//int getPotDeg(int sensor);
 
 /**********
 Methods
 **********/
 
-//int map(int value, int lowLimit, int highLimit, int newLow, int newHigh);
+//Re-maps a number from one range to another
+int map(int x, int in_min, int in_max, int out_min, int out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
