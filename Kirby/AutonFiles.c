22274a87931eb361fc063366833d9e97d0a53634
JavierO <javier.ortiz13@upr.edu>
2018-02-22 19:54:57 -0400

    added game map

--- Kirby/AutonFiles.c
+++ Kirby/AutonFiles.c
@@ -1,5 +1,14 @@
+#include "../MovementLogic.c"
+#include "../SensorLogic.h"
+
+//constants
+
+//globals
 
 void auton()
 {
 	//autonomous code
+
+  //here the code that takes the lcd inputs (whenever that is made)
+  //for now just the individual functions probably controlled by jumpers
 }
