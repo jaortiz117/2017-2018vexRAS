e70ac11e4768ca4587c52fc5451f2ebec5df893f
JavierO <javier.ortiz13@upr.edu>
2018-02-27 20:09:42 -0400

    reused old user control function

--- Kirby/baseRUMrobot.c
+++ Kirby/baseRUMrobot.c
@@ -253,26 +253,123 @@ task controller(){
     	AbortTimeSlice();
     }
 }
+
 task usercontrol()
 {
   // User control code here, inside the loop
 
-	//bool active = false;
-
-	StartTask(controller);
   while (true)
   {
+  	//base
+    	motor[baseTopLeft]= vexRT[Ch3] + vexRT[Ch1];
+    	motor[baseTopRight]= -vexRT[Ch3] + vexRT[Ch1];
+    	motor[baseBottomLeft]= vexRT[Ch3] + vexRT[Ch1];
+    	motor[baseBottomRight]= -vexRT[Ch3] + vexRT[Ch1];
 
-  	if(vexRT[Btn8L] == 1 && vexRT[Btn8R] == 1){
-  		StopTask(controller);
-  		stopAllMotors();
-  		wait1Msec(500);
-  		StartTask(controller);
-  		//break;
+
+    //arcadeControl(Ch3, Ch1);// didnt have to do anything found pre existing functions
+
+    	//transmition pneumatic
+		   	if (vexRT[Btn7U] == 1){
+		   		SensorValue[MGPiston] = 0;
+		   	}
+
+		   	else if (vexRT[Btn7D] == 1) {
+		   		SensorValue[MGPiston] = 1;
+		   	}
+
+
+    //base extra motor
+    if(SensorValue[MGPiston] == 1){
+   		 motor[baseMG]= -vexRT[Ch3];
   	}
 
-  	wait1Msec(10);
-  }
+    //moving goal mech
+ 		if(SensorValue[MGPiston] == 0){
+   		 if(vexRT[Btn7R] == 1){
+   		 motor[baseMG]= -127;
+   		}
 
-	//startTask(usercontrol);
+
+   		else if(vexRT[Btn7L] == 1){
+   		 motor[baseMG]= 127;
+   		}
+
+   		else{
+   			motor[baseMG]= 0;
+   		}
+  	}
+
+    //torre
+    if((vexRT[Btn6U] == 1) && (vexRT[Btn6D] == 0)){
+   	  motor(torreTopRight)= -127;
+   	  motor(torreTopLeft) = 127;
+    	motor(torreBottomLeft)= -127;
+    	motor(torreBottomRight)= 127;
+    }
+
+    else if((vexRT[Btn6D] == 1) && (vexRT[Btn6U] == 0)){
+    	motor(torreTopRight)= 127;
+    	 motor(torreTopLeft) = -127;
+    	motor(torreBottomLeft)= 127;
+    	motor(torreBottomRight)= -127;
+    }
+
+    else {
+    	motor(torreTopRight)= 0;
+    	 motor(torreTopLeft) = 0;
+    	motor(torreBottomLeft)= 0;
+    	motor(torreBottomRight)= 0;
+    }
+
+
+    //chainBar pivot
+    if((vexRT[Btn5U] == 1) && (vexRT[Btn5D] == 0)){
+   	  motor(chainPivot) = -127;
+    }
+
+    else if((vexRT[Btn5D] == 1) && (vexRT[Btn5U] == 0)){
+    	motor(chainPivot) = 127;
+    }
+
+    else {
+    	motor(chainPivot) = 0;
+    }
+
+
+    //cone intake
+    if(vexRT[Btn8D] == 1){
+    	SensorValue[claw] = 1;
+    }
+    else {
+    	SensorValue[claw] = 0;
+    }
+
+    //stack cones
+    	if(vexRT[Btn8U] == 1){
+
+				//startTask(stackCone);
+    		startTask(base);
+
+
+    		coneLift('U',700);
+    		hightControl(2100);
+    		coneLift('U',2900);
+    		stack(2100);
+    		SensorValue(claw) = 1;
+    		wait1Msec(250);
+    		hightControl(2100);
+    		SensorValue(claw) = 0;
+				wait1Msec(250);
+				coneLift('D',2350);
+
+
+
+				EndTimeSlice();
+
+
+
+    	}
+
+  }
 }
