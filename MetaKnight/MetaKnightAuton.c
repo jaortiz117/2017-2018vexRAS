a45b5910c713aa8006867939fa864009d70831e7
JavierO <javier.ortiz13@upr.edu>
2018-03-01 23:36:45 -0400

    added another part of the skills

--- MetaKnight/MetaKnightAuton.c
+++ MetaKnight/MetaKnightAuton.c
@@ -3,6 +3,7 @@
 void auton1()
 {
   //TODO make const for speeds, low, med, high, full
+  //add a 100 ms delay between every function
 
   //move towards goal
   move(inToTicks(24), 60);
@@ -39,13 +40,17 @@ void auton1()
 
   //move to position looking at north blue tile
   encoderRotate(45+90, 40);
+
+  //pickup and score blue goal
   move(inToTicks(6*12), 80);
   lift(DOWN);
   move(inToTicks(12), 40);
-  lift(UP);
-
-
-  //pickup and score blue goal
+  lift(UP);//pickup blue GOAL
+  move(inToTicks(-6*12), 80);
+  encoderRotate(180, 40);
+  move(inToTicks(12, 40));
+  lift(DOWN);//release goal
+  move(inToTicks(-12, 40));
 
   //position for south blue initialize
 
