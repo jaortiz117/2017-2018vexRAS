795380b395f8e2bd3eb4312eff882fb698de3439
JavierO <javier.ortiz13@upr.edu>
2018-03-02 00:24:15 -0400

    finished auton strategy implementation

--- MetaKnight/MetaKnightAuton.c
+++ MetaKnight/MetaKnightAuton.c
@@ -52,11 +52,21 @@ void auton1()
   lift(DOWN);//release goal
   move(inToTicks(-12, 40));
 
-  //position for south blue initialize
-
-  //pickup blue GOAL
-
-  //move towards red corner
+  //position for south blue
+  encoderRotate(180, 40);
+  //rotate towards center//needs work
+  encoderRotate(-45, 60);
+  //move towards two goals
+  move(inToTicks(48), 90);
+  lift(DOWN);
+  move(inToTicks(60), 40);
+  lift(UP);//pickup two goals
+  move(inToTicks(48), 90);//move towards other corner
+  move(inToTicks(12), 60);//walk over tube
+  lift(DOWN);
+  move(inToTicks(-12), 60);
 
   //park at south red tile
+  encoderRotate(-45-90, 40);
+  move(inToTicks(24*3), 90);
 }
